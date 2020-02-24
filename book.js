0c19c748a2118d430b4946302c6faa031ab61750
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2020-02-24 15:18:53 -0500

    intall bulma

--- book.js
+++ book.js
@@ -1,10 +1,20 @@
+let myLibrary = [];
+
 function Book(title, author, pages, status) {
     this.title = title
     this.author = author
     this.pages = pages
     this.status = status == "yes" ? "already read" : "not read yet"
     this.info = () => `${this.title} by ${this.author}, ${this.pages} pages, ${this.status}`
-} 
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book)
+  }
 
 myBook = new Book('Harry Potter', 'J.K. Rowling', 320, "yes")
-console.log(myBook.info())
+addBookToLibrary(myBook)
+console.log(myBook.info(), myLibrary)
+
+const element = <h1>Hello, world</h1>;
+render(element, document.getElementById('root'));
